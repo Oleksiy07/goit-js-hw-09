d415bb72941597997334b1611e05833f87ec392b
Oleksiy Komarnitskyi <oleksij.komarnitskuj@gmail.com>
2023-08-16 10:10:31 +0200

    Виконав завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const body = document.querySelector('body');
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+let timerId = null;
 
+startBtn.addEventListener('click', startChangeColor);
+
+function startChangeColor() {
+  startBtn.setAttribute('disabled', 'true');
+  timerId = setInterval(() => {
+    body.style.backgroundColor = `${getRandomHexColor()}`;
+  }, 1000);
+}
+
+stopBtn.addEventListener('click', stopChangeColor);
+
+function stopChangeColor() {
+  startBtn.removeAttribute('disabled');
+  clearInterval(timerId);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
